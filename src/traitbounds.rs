5ffd4023f3c79a50552b5b0c6d06f02363765f8d
lonnonjamesd <lemonjameslem@protonmail.com>
2022-04-22 11:50:20 -0400

    removed floor function

--- src/traitbounds.rs
+++ src/traitbounds.rs
@@ -1,4 +1,4 @@
-use std::iter::Step;
+use std::{iter::Step, ops::AddAssign};
 
 macro_rules! impl_trait {
     (impl $trait:ident for $($x:ident)*) => {
@@ -6,39 +6,19 @@ macro_rules! impl_trait {
     };
 }
 
-pub trait Unsigned: PartialEq + Ord + PartialOrd + Step + Sized {
-    fn floor(&self) -> Self {
-        self.floor()
-    }
-}
+pub trait Unsigned: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign  {}
 impl_trait!(impl Unsigned for usize u8 u16 u32 u64 u128);
 
-pub trait Signed: PartialEq + Ord + PartialOrd + Step + Sized {
-    fn floor(&self) -> Self {
-        self.floor()
-    }
-}
+pub trait Signed: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign {}
 impl_trait!(impl Signed for isize i8 i16 i32 i64 i128);
 
-pub trait Float: PartialEq + PartialOrd + Sized {
-    fn floor(&self) -> Self {
-        self.floor()
-    }
-}
+pub trait Float: PartialEq + PartialOrd + Sized + AddAssign {}
 impl_trait!(impl Float for f32 f64);
 
-pub trait Integer: PartialEq + Ord + PartialOrd + Sized {
-    fn floor(&self) -> Self {
-        self.floor()
-    }
-}
+pub trait Integer: PartialEq + Ord + PartialOrd + Sized + AddAssign  {}
 impl_trait!(impl Integer for usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
 
-pub trait Real: PartialEq + PartialOrd + Sized {
-    fn floor(&self) -> Self {
-        self.floor()
-    }
-}
+pub trait Real: PartialEq + PartialOrd + Sized + AddAssign  {}
 impl_trait!(impl Real for f32 f64 usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
 
 macro_rules! impl_from_trait {
