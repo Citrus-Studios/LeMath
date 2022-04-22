d2b953f6f15d5585a6ede58b6bea064effbffff8
lonnonjamesd <lemonjameslem@protonmail.com>
2022-04-22 13:05:12 -0400

    added trait alias to make my life easier

--- src/traitbounds.rs
+++ src/traitbounds.rs
@@ -1,4 +1,4 @@
-use std::{iter::Step, ops::{AddAssign, Sub, SubAssign, Div, Add, Mul}};
+use std::{iter::Step, ops::{AddAssign, Sub, SubAssign, Div, Add, Mul, MulAssign}};
 
 macro_rules! impl_trait {
     (impl $trait:ident for $($x:ident)*) => {
@@ -6,17 +6,19 @@ macro_rules! impl_trait {
     };
 }
 
-pub trait Unsigned: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign  + Mul + Add + Div + Sub + SubAssign {}
+pub trait NumberTraitBounds = PartialEq + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign + MulAssign;
+
+pub trait Unsigned: NumberTraitBounds + Ord {}
 impl_trait!(impl Unsigned for usize u8 u16 u32 u64 u128);
 
-pub trait Signed: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign{}
+pub trait Signed: NumberTraitBounds + Ord {}
 impl_trait!(impl Signed for isize i8 i16 i32 i64 i128);
 
-pub trait Float: PartialEq + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
+pub trait Float: NumberTraitBounds {}
 impl_trait!(impl Float for f32 f64);
 
-pub trait Integer: PartialEq + Ord + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
+pub trait Integer: NumberTraitBounds + Ord {}
 impl_trait!(impl Integer for usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
 
-pub trait Real: PartialEq + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
+pub trait Real: NumberTraitBounds {}
 impl_trait!(impl Real for f32 f64 usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
