12b41e90c4f05a0eece374bb426c94d0dd3f4217
lonnonjamesd <lemonjameslem@protonmail.com>
2022-04-22 12:48:40 -0400

    More Math traits

--- src/traitbounds.rs
+++ src/traitbounds.rs
@@ -1,4 +1,4 @@
-use std::{iter::Step, ops::AddAssign};
+use std::{iter::Step, ops::{AddAssign, Sub, SubAssign, Div, Add, Mul}};
 
 macro_rules! impl_trait {
     (impl $trait:ident for $($x:ident)*) => {
@@ -6,17 +6,17 @@ macro_rules! impl_trait {
     };
 }
 
-pub trait Unsigned: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign  {}
+pub trait Unsigned: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign  + Mul + Add + Div + Sub + SubAssign {}
 impl_trait!(impl Unsigned for usize u8 u16 u32 u64 u128);
 
-pub trait Signed: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign {}
+pub trait Signed: PartialEq + Ord + PartialOrd + Step + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign{}
 impl_trait!(impl Signed for isize i8 i16 i32 i64 i128);
 
-pub trait Float: PartialEq + PartialOrd + Sized + AddAssign {}
+pub trait Float: PartialEq + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
 impl_trait!(impl Float for f32 f64);
 
-pub trait Integer: PartialEq + Ord + PartialOrd + Sized + AddAssign  {}
+pub trait Integer: PartialEq + Ord + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
 impl_trait!(impl Integer for usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
 
-pub trait Real: PartialEq + PartialOrd + Sized + AddAssign  {}
+pub trait Real: PartialEq + PartialOrd + Sized + AddAssign + Mul + Add + Div + Sub + SubAssign {}
 impl_trait!(impl Real for f32 f64 usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128);
