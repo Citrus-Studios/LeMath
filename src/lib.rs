28b6428e55b2e093cb942f9875f7861238cf016f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-17 18:17:00 -0400

    added features

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,15 @@
 #![feature(trivial_bounds)]
 #![feature(trait_alias)]
 
-pub mod matrix_items;
 pub mod summation;
 pub mod traitbounds;
+
+#[cfg(feature = "linear-algebra")]
+pub mod matrix_items;
+#[cfg(feature = "linear-algebra")]
 pub mod vector_items;
 
+#[cfg(feature = "linear-algebra")]
 pub use matrix_items::matrix;
+#[cfg(feature = "linear-algebra")]
 pub use vector_items::vectors;
