99f852323b7d843a2fa5dd976e4dc4e43f6e7bdc
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-17 17:28:42 -0400

    Moved Things into their own folders

---
+++ src/Vector/vector_math.rs
@@ -0,0 +1,51 @@
+/// Dot Product
+impl<T: VectorGeneric<T>> Mul<Self> for Vector<T> {
+    type Output = T;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.dot_product(rhs)
+    }
+}
+
+/// Scalar Product
+impl<T: VectorGeneric<T>> Mul<T> for Vector<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        self.scalar_mul(rhs)
+    }
+}
+
+impl<T: VectorGeneric<T>> Mul<Matrix<T>> for Vector<T> {
+    type Output = Vector<T>;
+
+    fn mul(self, rhs: Matrix<T>) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl<T: VectorGeneric<T>> Add for Vector<T> {
+    type Output = Vector<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let larger = {
+            if self.len() > rhs.len() {
+                (self.len(), false);
+            }
+            (rhs.len(), true)
+        };
+        if larger.1 {
+            let mut cloned = self.clone();
+            for (i, _) in self.into_iter().enumerate() {
+                cloned[i] += rhs[i];
+            }
+            return cloned;
+        } else {
+            let mut cloned = rhs.clone();
+            for (i, _) in rhs.into_iter().enumerate() {
+                cloned[i] += self[i];
+            }
+            return cloned;
+        }
+    }
+}
--- src/Vector/vectors.rs
+++ src/Vector/vectors.rs
@@ -54,16 +54,6 @@ macro_rules! vector {
             temp_vec
         }
     };
-    ($intotype:ty, $($x:expr),*) => {
-        {
-            use $crate::vectors::{Vector, VectorType};
-            let mut temp_vec = Vector::new(VectorType::Row);
-            $(
-                temp_vec.push($x as $intotype);
-            )*
-            temp_vec
-        }
-    };
     ($($x:expr),*) => {
         {
             use $crate::vectors::{Vector, VectorType};
@@ -191,56 +181,6 @@ impl<T: VectorGeneric<T>, I: SliceIndex<[T]>> IndexMut<I> for Vector<T> {
     #[inline]
     fn index_mut(&mut self, index: I) -> &mut Self::Output {
         self.index_mut_(index)
-    }
-}
-
-impl<T: VectorGeneric<T>> Mul<Self> for Vector<T> {
-    type Output = T;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        self.dot_product(rhs)
-    }
-}
-
-impl<T: VectorGeneric<T>> Mul<T> for Vector<T> {
-    type Output = Self;
-
-    fn mul(self, rhs: T) -> Self::Output {
-        self.scalar_mul(rhs)
-    }
-}
-
-impl<T: VectorGeneric<T>> Mul<Matrix<T>> for Vector<T> {
-    type Output = Vector<T>;
-
-    fn mul(self, rhs: Matrix<T>) -> Self::Output {
-        rhs * self
-    }
-}
-
-impl<T: VectorGeneric<T>> Add for Vector<T> {
-    type Output = Vector<T>;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        let larger = {
-            if self.len() > rhs.len() {
-                (self.len(), false);
-            }
-            (rhs.len(), true)
-        };
-        if larger.1 {
-            let mut cloned = self.clone();
-            for (i, _) in self.into_iter().enumerate() {
-                cloned[i] += rhs[i];
-            }
-            return cloned;
-        } else {
-            let mut cloned = rhs.clone();
-            for (i, _) in rhs.into_iter().enumerate() {
-                cloned[i] += self[i];
-            }
-            return cloned;
-        }
     }
 }
 
