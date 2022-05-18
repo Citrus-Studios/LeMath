c82568b77ad67a1d2cc8c4c0a95755a53e8cad3b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-18 17:35:23 -0400

    moved folders into linear algebra folder

--- src/linear_algebra/mod.rs
+++ src/linear_algebra/mod.rs
@@ -1,15 +1,9 @@
-#[cfg(feature = "linear-algebra")]
 pub mod matrix_items;
-#[cfg(feature = "linear-algebra")]
 pub mod vector_items;
 
-#[cfg(feature = "linear-algebra")]
 pub use matrix_items::matrices;
-#[cfg(feature = "linear-algebra")]
 pub use vector_items::vectors;
 
-// The Linear Algebra Macros
-#[cfg(feature = "linear-algebra")]
+// The Macros
 pub use matrix_items::matrices::matrix;
-#[cfg(feature = "linear-algebra")]
 pub use vector_items::vectors::vector;
