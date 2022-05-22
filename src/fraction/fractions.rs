3c0b54e67d75b067ac0f974934cbde52a4d71e23
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-22 00:33:59 -0400

    Added from_float and fraction_test

--- src/fraction/fractions.rs
+++ src/fraction/fractions.rs
@@ -1,5 +1,6 @@
-use crate::helper::GCD;
+use crate::helper::{GetDecimal, GCD};
 
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Fraction {
     pub numerator: i128,
     pub denominator: i128,
@@ -11,6 +12,13 @@ impl Fraction {
             numerator,
             denominator,
         }
+        .reduce()
+    }
+    pub fn from_float(value: f64) -> Self {
+        let ten_pow = 10_u32.pow(format!("{}", value.get_decimal()).len() as u32);
+        let numerator = (value.trunc() as i128 * ten_pow as i128) + (value.get_decimal() as i128);
+        let denominator = ten_pow as i128;
+        return Fraction::new(numerator, denominator).reduce();
     }
     pub fn reduce(&self) -> Self {
         let mut numerator = self.numerator;
@@ -23,4 +31,11 @@ impl Fraction {
             denominator,
         };
     }
+}
+
+#[test]
+fn fraction_test() {
+    let x = Fraction::new(8291, 10000);
+    let y = Fraction::from_float(0.08291);
+    assert_eq!(x, y);
 }
