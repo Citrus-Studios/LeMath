0ef3f3ba7fb7b830a3d722180137974073423859
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-21 23:29:51 -0400

    Setup the rest of the Integral for implmenting the integration functions

--- src/calculus/integral_items/integrals.rs
+++ src/calculus/integral_items/integrals.rs
@@ -1,11 +1,17 @@
 use std::marker::PhantomData;
 
+pub trait IntFuncGen<I, R> = Fn<I, Output = R>;
+
 /// Calculus Integral
 /// Find the area underneath a curve
-pub struct Integral<A, F: Fn<A>> {
+pub struct Integral<I, R, F: IntFuncGen<I, R>> {
     integration: IntegrationType,
     equation: F,
-    _marker: PhantomData<A>,
+    from: f64,
+    to: f64,
+    sampling_rate: i64,
+    _marker: PhantomData<R>,
+    _marker2: PhantomData<I>,
 }
 
 /// Integration type for Integrals
@@ -14,13 +20,30 @@ pub enum IntegrationType {
     ReimannTrapozoidalSum,
 }
 
-impl<A, F: Fn<A>> Integral<A, F> {
-    pub fn new(equation: F) -> Self {
+impl<I, R, F: IntFuncGen<I, R>> Integral<I, R, F> {
+    pub fn new(from: f64, to: f64, equation: F) -> Self {
         Self {
             integration: IntegrationType::ReimannSum,
             equation,
+            from,
+            to,
+            sampling_rate: 32,
             _marker: PhantomData,
+            _marker2: PhantomData,
         }
+    }
+    pub fn set_sampling_rate(mut self, sampling_rate: i64) -> Self {
+        self.sampling_rate = sampling_rate;
+        self
+    }
+    pub fn reimann_sum(&self) -> R {
+        todo!();
+    }
+    pub fn build(&self) -> R {
+        return match self.integration {
+            IntegrationType::ReimannSum => self.reimann_sum(),
+            IntegrationType::ReimannTrapozoidalSum => todo!(),
+        };
     }
 }
 
@@ -28,5 +51,5 @@ impl<A, F: Fn<A>> Integral<A, F> {
 fn integral_test() {
     use crate::equation::equation;
 
-    let x = Integral::new(equation!(10x + 3));
+    let x = Integral::new(0.0, 3.0, equation!(x));
 }
