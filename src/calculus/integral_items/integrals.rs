dd8cc078db3f35a3e63ba19517dd1002f1b14429
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-22 12:03:49 -0400

    Rewriting Reimann Sum algo

--- src/calculus/integral_items/integrals.rs
+++ src/calculus/integral_items/integrals.rs
@@ -1,15 +1,15 @@
 use std::marker::PhantomData;
 
-use crate::{fraction::fractions::Fraction, summation::sum_extra, traitbounds::Real};
+use crate::fraction::fractions::Fraction;
 
 /// Calculus Integral
 /// Find the area underneath a curve\
 #[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub struct Integral {
     integration: IntegrationType,
-    equation: fn(f64) -> f64,
-    from: f64,
-    to: f64,
+    equation: fn(Fraction) -> Fraction,
+    from: Fraction,
+    to: Fraction,
     sampling_rate: i64,
 }
 
@@ -21,12 +21,12 @@ pub enum IntegrationType {
 }
 
 impl Integral {
-    pub fn new(from: f64, to: f64, equation: fn(f64) -> f64) -> Self {
+    pub fn new(from: f64, to: f64, equation: fn(Fraction) -> Fraction) -> Self {
         Self {
             integration: IntegrationType::ReimannSum,
             equation,
-            from,
-            to,
+            from: from.into(),
+            to: to.into(),
             sampling_rate: 32,
         }
     }
@@ -34,17 +34,12 @@ impl Integral {
         self.sampling_rate = sampling_rate;
         self
     }
-    pub fn reimann_sum(&self) -> f64 {
-        let dx = Fraction::new((self.to - self.from) as i128, self.sampling_rate as i128);
-        println!("{dx}");
-        let mut res = Fraction::from(0.0);
-        for x in 1..=self.sampling_rate {
-            println!("{x} {res}");
-            res += dx * Fraction::from(self.equation.call((f64::from(dx) * x as f64,)));
-        }
-        return f64::from(res);
+    pub fn reimann_sum(&self) -> Fraction {
+        let dx = (self.to - self.from) / self.sampling_rate.into();
+
+        todo!();
     }
-    pub fn build(&self) -> f64 {
+    pub fn build(&self) -> Fraction {
         return match self.integration {
             IntegrationType::ReimannSum => self.reimann_sum(),
             IntegrationType::ReimannTrapozoidalSum => todo!(),
@@ -54,9 +49,10 @@ impl Integral {
 
 #[test]
 fn integral_test() {
-    let x = Integral::new(0.0, 3.0, |x| -> f64 {
+    let x = Integral::new(0.0, 3.0, |x| {
         return x;
     })
+    .set_sampling_rate(4)
     .build();
-    assert_eq!(4.5, x);
+    assert_eq!(4.5, x.into());
 }
