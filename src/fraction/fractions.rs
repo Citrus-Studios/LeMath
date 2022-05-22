7ef1e6e853d3f6b665b306dee84c67c9ffb75d0f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-22 00:22:02 -0400

    Ground work for fractions

--- src/fraction/fractions.rs
+++ src/fraction/fractions.rs
@@ -1,1 +1,13 @@
+pub struct Fraction {
+    numerator: i128,
+    denominator: i128,
+}
 
+impl Fraction {
+    pub fn new(numerator: i128, denominator: i128) -> Self {
+        Self {
+            numerator,
+            denominator,
+        }
+    }
+}
