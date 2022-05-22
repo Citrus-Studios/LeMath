f321c79efa3da5201628de763dcac6b4472edb7b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-22 00:10:16 -0400

    Implemented most of the reimann sum

--- src/calculus/integral_items/integrals.rs
+++ src/calculus/integral_items/integrals.rs
@@ -1,45 +1,46 @@
-use std::marker::PhantomData;
-
-pub trait IntFuncGen<I, R> = Fn<I, Output = R>;
-
 /// Calculus Integral
-/// Find the area underneath a curve
-pub struct Integral<I, R, F: IntFuncGen<I, R>> {
+/// Find the area underneath a curve\
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+pub struct Integral {
     integration: IntegrationType,
-    equation: F,
+    equation: fn(f64) -> f64,
     from: f64,
     to: f64,
     sampling_rate: i64,
-    _marker: PhantomData<R>,
-    _marker2: PhantomData<I>,
 }
 
 /// Integration type for Integrals
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub enum IntegrationType {
     ReimannSum,
     ReimannTrapozoidalSum,
 }
 
-impl<I, R, F: IntFuncGen<I, R>> Integral<I, R, F> {
-    pub fn new(from: f64, to: f64, equation: F) -> Self {
+impl Integral {
+    pub fn new(from: f64, to: f64, equation: fn(f64) -> f64) -> Self {
         Self {
             integration: IntegrationType::ReimannSum,
             equation,
             from,
             to,
             sampling_rate: 32,
-            _marker: PhantomData,
-            _marker2: PhantomData,
         }
     }
     pub fn set_sampling_rate(mut self, sampling_rate: i64) -> Self {
         self.sampling_rate = sampling_rate;
         self
     }
-    pub fn reimann_sum(&self) -> R {
-        todo!();
+    pub fn reimann_sum(&self) -> f64 {
+        let dx = (self.to - self.from) / self.sampling_rate as f64;
+        println!("{dx}");
+        let mut res = 0.0;
+        for x in 1..=self.sampling_rate {
+            println!("{x} {res}");
+            res += dx * self.equation.call((dx * x as f64,));
+        }
+        return res;
     }
-    pub fn build(&self) -> R {
+    pub fn build(&self) -> f64 {
         return match self.integration {
             IntegrationType::ReimannSum => self.reimann_sum(),
             IntegrationType::ReimannTrapozoidalSum => todo!(),
@@ -49,7 +50,9 @@ impl<I, R, F: IntFuncGen<I, R>> Integral<I, R, F> {
 
 #[test]
 fn integral_test() {
-    use crate::equation::equation;
-
-    let x = Integral::new(0.0, 3.0, equation!(x));
+    let x = Integral::new(0.0, 3.0, |x| -> f64 {
+        return x;
+    })
+    .build();
+    assert_eq!(4.5, x);
 }
