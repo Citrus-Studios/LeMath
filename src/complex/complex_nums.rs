1a09f2e7aff3e9f4b37f67ff4095fbbb3f00625b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 21:53:21 -0400

    Removed Mutable + abs_sq

--- src/complex/complex_nums.rs
+++ src/complex/complex_nums.rs
@@ -10,8 +10,15 @@ impl Complex {
     pub fn new(real: Fraction, imaginary: Fraction) -> Self {
         Self { real, imaginary }
     }
-    pub fn conjugate(&mut self) -> Self {
-        self.imaginary = -self.imaginary;
-        self.clone()
+    pub fn conjugate(self) -> Self {
+        let mut self_clone = self.clone();
+        self_clone.imaginary = -self_clone.imaginary;
+        self_clone
+    }
+    pub fn abs_sq(self) -> Self {
+        return Complex::new(
+            self.real * self.real + self.imaginary * self.imaginary,
+            0.into(),
+        );
     }
 }
