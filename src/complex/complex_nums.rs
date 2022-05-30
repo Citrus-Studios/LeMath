7ea5ac19415b813fd41a6e78ea30e0e59259e074
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 21:34:34 -0400

    Added new function

--- src/complex/complex_nums.rs
+++ src/complex/complex_nums.rs
@@ -4,3 +4,9 @@ pub struct Complex {
     real: Fraction,
     imaginary: Fraction,
 }
+
+impl Complex {
+    pub fn new(real: Fraction, imaginary: Fraction) -> Self {
+        Self { real, imaginary }
+    }
+}
