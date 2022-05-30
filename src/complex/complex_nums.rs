bfd74c77dba778fa7b72b299c569359a421e20b2
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 21:44:21 -0400

    Added Conjugate

--- src/complex/complex_nums.rs
+++ src/complex/complex_nums.rs
@@ -1,5 +1,6 @@
 use crate::fractions_num::Fraction;
 
+#[derive(Clone, Copy, Debug)]
 pub struct Complex {
     pub(crate) real: Fraction,
     pub(crate) imaginary: Fraction,
@@ -8,5 +9,9 @@ pub struct Complex {
 impl Complex {
     pub fn new(real: Fraction, imaginary: Fraction) -> Self {
         Self { real, imaginary }
+    }
+    pub fn conjugate(&mut self) -> Self {
+        self.imaginary = -self.imaginary;
+        self.clone()
     }
 }
