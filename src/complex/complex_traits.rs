e8685b02fbcef38af727884a50bd94340bd7fdc1
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 21:40:40 -0400

    Added Multiplication for Complex Numbers

--- src/complex/complex_traits.rs
+++ src/complex/complex_traits.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Mul, Sub};
 
 use super::complex_nums::Complex;
 
@@ -15,5 +15,16 @@ impl Sub<Complex> for Complex {
 
     fn sub(self, rhs: Self) -> Self::Output {
         Complex::new(self.real - rhs.real, self.imaginary - rhs.imaginary)
+    }
+}
+
+impl Mul<Complex> for Complex {
+    type Output = Complex;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Complex::new(
+            (self.real * rhs.real) - (self.imaginary * rhs.imaginary),
+            (self.real * rhs.imaginary) + (self.imaginary * rhs.real),
+        )
     }
 }
