d08a970d8e0777a7bb377a26fb38f613d6836bcc
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-03 16:31:45 -0400

    Moved Traits to seperate file + made contents pub(crate)

--- src/linear_algebra/matrix_items/matrices.rs
+++ src/linear_algebra/matrix_items/matrices.rs
@@ -1,9 +1,4 @@
-use std::{fmt::Display, ops::Mul};
-
-use crate::{
-    linear_algebra::vectors::{Vector, VectorGeneric},
-    vector,
-};
+use crate::linear_algebra::vectors::{Vector, VectorGeneric};
 
 #[macro_export]
 macro_rules! matrix {
@@ -56,7 +51,7 @@ macro_rules! matrix {
 
 #[derive(Debug)]
 pub struct Matrix<T: VectorGeneric<T>> {
-    contents: Vec<Vector<T>>,
+    pub(crate) contents: Vec<Vector<T>>,
 }
 
 impl<T: VectorGeneric<T>> Matrix<T> {
@@ -95,75 +90,6 @@ impl<T: VectorGeneric<T>> Matrix<T> {
             }
         }
         false
-    }
-}
-
-impl<T: VectorGeneric<T>> Display for Matrix<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.len() == 1 {
-            write!(f, "{}", self.contents[0]).unwrap();
-        } else {
-            if self.contents.len() == 1 {
-                write!(f, "{}", format!("{:?}", self.contents).replace(',', "")).unwrap();
-            } else if self.contents.len() == 2 {
-                write!(
-                    f,
-                    "⎡ {} ⎤\n",
-                    format!("{}", self.contents[0])
-                        .replace("[", "")
-                        .replace("]", "")
-                )
-                .unwrap();
-                write!(
-                    f,
-                    "⎣ {} ⎦",
-                    format!("{}", self.contents[1])
-                        .replace("[", "")
-                        .replace("]", "")
-                )
-                .unwrap();
-            } else {
-                write!(
-                    f,
-                    "⎡ {} ⎤\n",
-                    format!("{}", self.contents[0])
-                        .replace("[", "")
-                        .replace("]", "")
-                )
-                .unwrap();
-                for x in 1..self.contents.len() - 1 {
-                    write!(
-                        f,
-                        "⎢ {} ⎥\n",
-                        format!("{}", self.contents[x])
-                            .replace("[", "")
-                            .replace("]", "")
-                    )
-                    .unwrap();
-                }
-                write!(
-                    f,
-                    "⎣ {} ⎦",
-                    format!("{}", self.contents[self.contents.len() - 1])
-                        .replace("[", "")
-                        .replace("]", "")
-                )
-                .unwrap();
-            }
-        }
-        f.write_str("")
-    }
-}
-
-impl<T: VectorGeneric<T>> Mul<Vector<T>> for Matrix<T> {
-    type Output = Vector<T>;
-
-    fn mul(self, rhs: Vector<T>) -> Self::Output {
-        let mut temp_vec = vector![];
-        for x in 0..self.len() {
-            temp_vec.push(self.contents[x].clone() * rhs.clone());
-        }
-        temp_vec
     }
 }
 
