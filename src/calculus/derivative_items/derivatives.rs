5f7d49fb9b2505a8a2a5f27b54e3e96b1887949e
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-05 12:33:50 -0400

    Creating Derivative from the Pairs<Rule>

--- src/calculus/derivative_items/derivatives.rs
+++ src/calculus/derivative_items/derivatives.rs
@@ -1,1 +1,13 @@
-pub struct Derivative {}
+use pest::iterators::Pairs;
+
+use crate::equation::Rule;
+
+pub struct Derivative<'a> {
+    equation: Pairs<'a, Rule>,
+}
+
+impl<'a> Derivative<'a> {
+    pub fn new(equation: Pairs<'a, Rule>) -> Self {
+        Self { equation }
+    }
+}
