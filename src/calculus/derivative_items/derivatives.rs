fbbce1e8a4bda8fb7d6f7aa813cd3a3de325df48
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-05 12:39:29 -0400

    Building function added

--- src/calculus/derivative_items/derivatives.rs
+++ src/calculus/derivative_items/derivatives.rs
@@ -1,6 +1,6 @@
 use pest::iterators::Pairs;
 
-use crate::equation::Rule;
+use crate::{equation::Rule, fractions_num::Fraction};
 
 pub struct Derivative<'a> {
     equation: Pairs<'a, Rule>,
@@ -9,5 +9,8 @@ pub struct Derivative<'a> {
 impl<'a> Derivative<'a> {
     pub fn new(equation: Pairs<'a, Rule>) -> Self {
         Self { equation }
+    }
+    pub fn build() -> fn(Fraction) -> Fraction {
+        todo!()
     }
 }
